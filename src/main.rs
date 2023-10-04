4fe6ad46b84e87e5aa560909e98a80ca2ec36412
Moosa Junaadh <junaadh.02@gmail.com>
2023-10-04 21:13:25 +0800

    added double/triple fault handling

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,14 @@ pub extern "C" fn _start() -> ! {
 
     rust_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    // x86_64::instructions::interrupts::int3();
+    // unsafe {
+    //     *(0xdeadbeef as *mut u8) = 42;
+    // };
+    fn stack_overflow() {
+        stack_overflow();
+    }
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
